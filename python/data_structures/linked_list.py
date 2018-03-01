9365505f4d949e12a44a524c1a0f5a54f098bab6
Gary Hou <hguochen@gmail.com>
2018-02-28 20:02:50 -0800

    implement linked list

--- python/data_structures/linked_list.py
+++ python/data_structures/linked_list.py
@@ -35,6 +35,26 @@ class LinkedList(object):
         node = Node(value)
         curr.next = node
         return
+
+    def delete(self, value):
+        """
+        Delete the node which contains the value
+        """
+        prev = None
+        curr = self.head
+        while curr is not None:
+            if curr.value == value:
+                if prev is None:
+                    self.head = self.head.next
+                    return
+                prev.next = curr.next
+                del curr
+                break
+            prev = curr
+            curr = curr.next
+        return
+
+
         
     def print_list(self):
         curr = self.head
@@ -43,10 +63,21 @@ class LinkedList(object):
             curr = curr.next
         return
 
+    def find(self, value):
+        curr = self.head
+        while curr is not None:
+            if curr.value == value:
+                return curr
+            curr = curr.next
+        return False
+
 def main():
     linked_list = LinkedList(1)
     linked_list.insert_back(2)
     linked_list.insert_back(3)
+    linked_list.print_list()
+    print linked_list.find(2)
+    linked_list.delete(2)
     linked_list.print_list()
 
 
