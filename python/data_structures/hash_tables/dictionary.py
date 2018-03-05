1580583a14e5c530f95882b16cc143da336d41ab
Gary Hou <hguochen@gmail.com>
2018-03-05 13:38:28 -0800

    move files

--- python/data_structures/hash_tables/dictionary.py
+++ python/data_structures/hash_tables/dictionary.py
@@ -9,6 +9,35 @@ stuff = {
     'height': 4 * 12
 }
 
+# set key to value
+stuff['hair_color'] = 'black'
+
+# check if key is in dict
+print 'hair_color' in stuff # true
+
+# delete key and its value from dict. Raises KeyError if key is not in map
+del stuff['hair_color']
+print 'hair_color' in stuff # false
+
+# return an iterator over the keys of the dictionary, this is a shortcut for iterkeys()
+iterator = iter(stuff)
+
+# return a copy of the dictionary's list of (key, value) pairs
+print "items are: ",
+print stuff.items()
+
+# clear all items from the dictionary
+# stuff.clear()
+
+# update the dictionary with key value pairs or another dictionary, return None
+a = {'a': 1}
+b = {'b': 2}
+a.update(b) # a = {'a': 1, 'b': 2}
+a.update(c=3, d=4)
+
+# if key is in dictionary. remvoe and return its value, else return default 
+stuff.pop('height')
+
 # you can also build a dictionary directly from sequence of key value pairs
 new_stuff = dict([('sape', 4139), ('guido', 4217), ('jack', 4098)])
 
