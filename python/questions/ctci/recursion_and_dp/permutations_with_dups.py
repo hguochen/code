7cc0ed82addaae6caadb0a0fbca10fd796a4ecb2
Gary Hou <hguochen@gmail.com>
2018-04-07 17:36:35 -0700

    alternate implementation of perm with dups

--- python/questions/ctci/recursion_and_dp/permutations_with_dups.py
+++ python/questions/ctci/recursion_and_dp/permutations_with_dups.py
@@ -26,9 +26,40 @@ def perm_util(arr, start, end, res):
         perm_util(arr, start+1, end, res)
         arr[i], arr[start] = arr[start], arr[i]
 
+def permutations_with_dups2(string):
+    if len(string) < 1:
+        return []
+    result = []
+    table = build_freq_table(string)
+    perm_util2(table, "", len(string), result)
+    return result
+
+def perm_util2(table, prefix, remaining, result):
+    if remaining == 0:
+        result.append(prefix)
+        return
+    # try remaining letters for next char, and generate remaining permutations
+    for char in table.keys():
+        if table[char] > 0:
+            table[char] -= 1
+            perm_util2(table, prefix + char, remaining - 1, result)
+            table[char] += 1
+
+def build_freq_table(string):
+    table = {}
+    if len(string) < 1:
+        return table
+    for char in string:
+        if char in table.keys():
+            table[char] += 1
+        else:
+            table[char] = 1
+    return table
 
 if __name__ == '__main__':
     str1 = "abc"
     str2 = "abb"
     print permutations_with_dups(str1)
     print permutations_with_dups(str2)
+    print permutations_with_dups2(str1)
+    print permutations_with_dups2(str2)
