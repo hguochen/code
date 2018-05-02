e5ba89de5aa0385b65002fbcd371fc12156636a4
Gary Hou <hguochen@gmail.com>
2018-05-02 10:32:50 +0800

    adding java programs

---
+++ java/java_the_complete_reference/chapter2/Example.java
@@ -0,0 +1,7 @@
+class Example {
+    public static void main(String args[]) {
+        /* This is a multiline comment */
+        // This is a single line comment
+        System.out.println("This is a simple Java program.");
+    }
+}
---
+++ java/java_the_complete_reference/chapter2/Example2.java
@@ -0,0 +1,14 @@
+class Example2 {
+    public static void main(String args[]) {
+        int num; // declares a variable called num
+        num = 100; // assigns the value 100 to num
+        System.out.println("This is num: " + num);
+        num = num * 2;
+        System.out.println("Value of num * 2 is: ");
+        System.out.println(num);
+
+        for (int i=0; i < 10; i++) {
+            System.out.println("Value of i: " + i);
+        }
+    }
+}
