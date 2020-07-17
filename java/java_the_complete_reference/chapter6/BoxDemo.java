22735532555fdf4022049da8f68b47a133140d15
Gary Hou <hguochen@gmail.com>
2020-07-16 17:00:12 -0700

    add boxdemo exercise

--- java/java_the_complete_reference/chapter6/BoxDemo.java
+++ java/java_the_complete_reference/chapter6/BoxDemo.java
@@ -2,43 +2,27 @@ class Box {
     double width;
     double height;
     double depth;
-
-    Box(double w, double h, double d) {
-        width = w;
-        height = h;
-        depth = d;
-    }
-
-    Box() {
-        width = -1;
-        height = -1;
-        depth = -1;
-    }
-
-    // constructor used when cube is created
-    Box(double len) {
-        width = height = depth = len;
-    }
-
-    double volume() {
-        return width * height * depth;
-    }
 }
 
 class BoxDemo {
     public static void main(String args[]) {
-        Box myBox = new Box(); 
+        Box myBox = new Box();
+        Box myBox2 = new Box();
         double vol;
+        double vol2;
 
         myBox.width = 10;
         myBox.height = 20;
         myBox.depth = 15;
 
-        vol = myBox.width * myBox.height * myBox.depth;
+        myBox2.width = 5;
+        myBox2.height = 30;
+        myBox2.depth = 20;
 
+        vol = myBox.width * myBox.height * myBox.depth;
         System.out.println("Volume is " + vol);
 
-        Box myBox2 = new Box(2);
-        System.out.println("Volume is " + myBox2.volume());
+        vol2 = myBox2.width * myBox2.height * myBox2.depth;
+        System.out.println("Volume is " + vol2);
     }
 }
